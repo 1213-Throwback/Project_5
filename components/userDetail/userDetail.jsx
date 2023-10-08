0bb1b0a6c5e0168e3391d3f003ffda01987495d1
Vulcran <104237476+Vulcran@users.noreply.github.com>
2023-10-08 18:46:25 -0400

    Altered the code so that it could be built correctly. Displayed the users on the side bar and created links with user/:id

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -12,15 +12,19 @@ class UserDetail extends React.Component {
   constructor(props) {
     super(props);
   }
-
-  render() {
-    return (
-      <Typography variant="body1">
+/*
         This should be the UserDetail view of the PhotoShare app. Since
         it is invoked from React Router the params from the route will be
         in property match. So this should show details of user:
         {this.props.match.params.userId}. You can fetch the model for the
         user from window.models.userModel(userId).
+ */
+  render() {
+
+
+    return (
+        <Typography variant="body1">
+
       </Typography>
     );
   }
--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
+    Divider, Link,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
 }
-from '@mui/material';
+    from '@mui/material';
 import './userList.css';
+import {Box} from "@mui/system";
 
 /**
  * Define UserList, a React component of project #5
@@ -18,33 +19,36 @@ class UserList extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
-        <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
-        </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
-      </div>
-    );
+
+      return (
+          <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+              {window.models.userListModel().map((item, index) => (
+                  <React.Fragment key={item._id}>
+                      <ListItem>
+                          <ListItemText key={index} primary={<a href={"users/:" + item._id}>{item.first_name}</a>}/>
+                      </ListItem>
+                      <Divider />
+                  </React.Fragment>
+              ))}
+          </Box>
+      );
   }
 }
+
+
+/*
+        <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+              {window.models.userListModel().map((item, index) => (
+                  <React.Fragment key={item._id}>
+                      <ListItem>
+                        <ListItemText key={index} primary={<a href={"users/:" + item._id}>{item.first_name}</a>}/>
+                      </ListItem>
+                      <Divider />
+                  </React.Fragment>
+              ))}
+          </Box>
+ */
+
+
 
 export default UserList;
