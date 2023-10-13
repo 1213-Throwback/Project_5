68989c60f61593f3eb239f3eca0a0622ad6a27ae
albert-bastakoti <143361459+albert-bastakoti@users.noreply.github.com>
2023-10-13 15:00:12 -0400

    I utilized text fields and boxes to clean up the user details.

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import {
     Button,
-    Typography
+    Typography,
+    TextField,
+    Box,
 } from '@mui/material';
 import './userDetail.css';
 import fetchModel from '../../lib/fetchModelData';
 
-
-/**
- * Define UserDetail, a React component of project #5
- */
 class UserDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        user:null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+        };
+    }
 
-/*
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
- */
     componentDidMount() {
         const new_user_id = this.props.match.params.userId;
         this.handleUserChange(new_user_id);
@@ -33,43 +24,71 @@ class UserDetail extends React.Component {
     componentDidUpdate() {
         const new_user_id = this.props.match.params.userId;
         const current_user_id = this.state.user?._id;
-        if (current_user_id  !== new_user_id){
+        if (current_user_id !== new_user_id) {
             this.handleUserChange(new_user_id);
         }
     }
 
-    handleUserChange(user_id){
+    handleUserChange(user_id) {
         fetchModel("/user/" + user_id)
-            .then((response) =>
-            {
+            .then((response) => {
                 const new_user = response.data;
                 this.setState({
-                    user: new_user
+                    user: new_user,
                 });
-                const main_content = "User Details for " + new_user.first_name + " " + new_user.last_name;
             });
     }
 
-  render() {
-        const {user} = this.state;
+    render() {
+        const { user } = this.state;
 
-        if(!user){
+        if (!user) {
             return null;
         }
 
-    const photosLink = "#/photos/" + user._id;
+        const photosLink = "#/photos/" + user._id;
 
-    return (
-        <div className = "user-detail">
-            <Button href={photosLink}>User Photos</Button>
-            <Typography variant = 'h5'>{`${user.first_name} ${user.last_name}`}</Typography>
-            <Typography variant = 'body1'>{`Location: ${user.location}`}</Typography>
-            <Typography variant = 'body1'>{`Description: ${user.description}`}</Typography>
-            <Typography variant = 'body1'>{`Occupation: ${user.occupation}`}</Typography>
-            <Typography variant = 'body1'>{`User ID: ${user._id}`}</Typography>
-        </div>
-    );
-  }
+        return (
+            <div className="user-detail">
+                <Button href={photosLink}>User Photos</Button>
+                <Typography variant='h5'>{`${user.first_name} ${user.last_name}`}</Typography>
+                <Box>
+                    <TextField
+                        label="Location"
+                        variant="outlined"
+                        value={user.location}
+                        fullWidth
+                        disabled
+                        style={{ marginBottom: '10px' }} // Add spacing
+                    />
+                    <TextField
+                        label="Description"
+                        variant="outlined"
+                        value={user.description}
+                        fullWidth
+                        disabled
+                        style={{ marginBottom: '10px' }} // Add spacing
+                    />
+                    <TextField
+                        label="Occupation"
+                        variant="outlined"
+                        value={user.occupation}
+                        fullWidth
+                        disabled
+                        style={{ marginBottom: '10px' }} // Add spacing
+                    />
+                    <TextField
+                        label="User ID"
+                        variant="outlined"
+                        value={user._id}
+                        fullWidth
+                        disabled
+                        style={{ marginBottom: '10px' }} // Add spacing
+                    />
+                </Box>
+            </div>
+        );
+    }
 }
 
 export default UserDetail;
